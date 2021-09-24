f90ea816f47285e820b2853256e89736b673bf40
Brendan Luke <31905278+BrendanLuke15@users.noreply.github.com>
2021-09-24 14:56:42 -0400

    Add files via upload

--- outputSelection.js
+++ outputSelection.js
@@ -1,6 +1,6 @@
 /*
 By: Brendan Luke
-Date: September 21, 2021
+Date: September 24, 2021
 Scope: script and functions to configure the webpage based on selected operation
 */
 
@@ -8,12 +8,18 @@ function encodeSelect() {
     // styling & display
     document.getElementById("encode").className = "active";
     document.getElementById("decode").className = "";
-    //document.getElementById("userPrompt").style.display = "none";
+    document.getElementById("encoderInput").style.display = "inline-block";
+    document.getElementById("encoderOutput").style.display = "inline-block";
+    document.getElementById("decoderInput").style.display = "none";
+    document.getElementById("decoderOutput").style.display = "none";
 }
 
 function decodeSelect() {
     // styling & display
     document.getElementById("encode").className = "";
     document.getElementById("decode").className = "active";
-    //document.getElementById("userPrompt").style.display = "none";
+    document.getElementById("encoderInput").style.display = "none";
+    document.getElementById("encoderOutput").style.display = "none";
+    document.getElementById("decoderInput").style.display = "inline-block";
+    document.getElementById("decoderOutput").style.display = "inline-block";
 }
